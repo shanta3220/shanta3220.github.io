55255e6812f3ca2566a959948071ed77b67042f2
NJShanta <njshanta.ca@gmail.com>
2024-12-19 17:42:20 -0500

    add content

---
+++ src/scripts/content.js
@@ -0,0 +1,16 @@
+export const projects = [
+  {
+    title: "JiffyGames",
+    description:
+      "A browser-based gaming platform featuring 5–6 games and community features.",
+    tech: ["React", "Sass", "MySQL"],
+    link: "https://github.com/shanta3220/jiffygames",
+  },
+  {
+    title: "Just for Laughs",
+    description:
+      "A personalized joke generator built during BrainStation Hackathon.",
+    tech: ["React", "Express", "MySQL"],
+    link: "https://github.com/shanta3220/just-for-laughs",
+  },
+];
