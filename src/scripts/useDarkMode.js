6b7f3e7c1f7e3375dfc32c50d655c8251032f156
NJShanta <njshanta.ca@gmail.com>
2024-12-19 18:02:39 -0500

    fix the package and add base for components

---
+++ src/scripts/useDarkMode.js
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react";
+
+const useDarkMode = () => {
+  const [theme, setTheme] = useState("light");
+
+  useEffect(() => {
+    const storedTheme = localStorage.getItem("theme") || "light";
+    setTheme(storedTheme);
+    document.documentElement.setAttribute("data-theme", storedTheme);
+  }, []);
+
+  const toggleTheme = () => {
+    const newTheme = theme === "light" ? "dark" : "light";
+    setTheme(newTheme);
+    document.documentElement.setAttribute("data-theme", newTheme);
+    localStorage.setItem("theme", newTheme);
+  };
+
+  return { theme, toggleTheme };
+};
+
+export default useDarkMode;
