1aa24cae50cc4ca2a98a8c5192ec32cefe148ee7
NJShanta <njshanta.ca@gmail.com>
2024-12-19 19:30:36 -0500

    use variable in hero's style

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,16 +1,19 @@
-import React from "react";
 import "./Hero.scss";
 
-const Hero = () => (
-  <section className="hero" id="home">
-    <div className="hero__content">
-      <h1 className="hero__title">Hi, I'm Nusrat Jahan Shanta</h1>
-      <p className="hero__subtitle">Full-Stack Developer | Game Developer</p>
-      <a href="#contact" className="hero__contact">
-        Contact Me
-      </a>
-    </div>
-  </section>
-);
+function Hero() {
+  return (
+    <section className="hero" id="home">
+      <div className="hero__content">
+        <h1 className="hero__title">Hi, I'm Nusrat Jahan Shanta</h1>
+        <p className="hero__subtitle">
+          Full-Stack Developer | Game Enthusiast | Problem Solver
+        </p>
+        <a href="#contact" className="hero__contact">
+          Contact Me
+        </a>
+      </div>
+    </section>
+  );
+}
 
 export default Hero;
