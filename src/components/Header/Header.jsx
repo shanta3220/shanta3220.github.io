2345d9e6893ed6adb3ff72c081d22e3b014573be
NJShanta <njshanta.ca@gmail.com>
2024-12-19 19:26:45 -0500

    Add theme toggle to header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,28 +1,41 @@
-import React from "react";
+import { useState } from "react";
+import ThemeToggle from "../ThemeToggle/ThemeToggle";
 import "./Header.scss";
 
-const Header = () => (
-  <header className="header">
-    <nav className="header__nav">
-      <ul className="header__list">
-        <li className="header__item">
-          <a href="#about">About</a>
-        </li>
-        <li className="header__item">
-          <a href="#skills">Skills</a>
-        </li>
-        <li className="header__item">
-          <a href="#experience">Experience</a>
-        </li>
-        <li className="header__item">
-          <a href="#projects">Projects</a>
-        </li>
-        <li className="header__item">
-          <a href="#contact">Contact</a>
-        </li>
-      </ul>
-    </nav>
-  </header>
-);
+function Header() {
+  const [theme, setTheme] = useState("light");
+
+  const changeTheme = (newTheme) => {
+    document.documentElement.setAttribute("data-theme", newTheme);
+    setTheme(newTheme);
+  };
+
+  return (
+    <header className="header">
+      <nav className="header__nav">
+        <ul className="header__list">
+          <li className="header__item">
+            <a href="#about">About</a>
+          </li>
+          <li className="header__item">
+            <a href="#skills">Skills</a>
+          </li>
+          <li className="header__item">
+            <a href="#experience">Experience</a>
+          </li>
+          <li className="header__item">
+            <a href="#projects">Projects</a>
+          </li>
+          <li className="header__item">
+            <a href="#contact">Contact</a>
+          </li>
+          <li className="header__item">
+            <ThemeToggle currentTheme={theme} onChangeTheme={changeTheme} />
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
 
 export default Header;
