bcf99ebea21cfc7d8341889191280460bde2abf3
NJShanta <njshanta.ca@gmail.com>
2024-12-20 18:21:53 -0500

    Add cusdom cursor and fixed background animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import CustomCursor from "./components/CustomCursor/CustomCursor";
+import AnimatedBackground from "./components/AnimatedBackground/AnimatedBackground";
+import MatrixCanvasBackground from "./components/MatrixCanvasBackground/MatrixCanvasBackground";
 import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
 import About from "./components/About/About";
@@ -7,14 +10,14 @@ import Experience from "./components/Experience/Experience";
 import Projects from "./components/Projects/Projects";
 import Contact from "./components/Contact/Contact";
 import Footer from "./components/Footer/Footer";
-import MatrixCanvasBackground from "./components/MatrixCanvasBackground/MatrixCanvasBackground";
 
 import "./app.scss";
 
 function App() {
   return (
     <div>
-      <div className="animated-background"></div>
+      <CustomCursor />
+      <AnimatedBackground />
       <MatrixCanvasBackground />
       <div className="content">
         <Header />
