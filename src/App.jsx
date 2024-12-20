f88302977306a179a5bf60c8d4c74584f22c86b0
NJShanta <njshanta.ca@gmail.com>
2024-12-19 19:24:29 -0500

    Add theme and header

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
 import About from "./components/About/About";
@@ -7,6 +6,7 @@ import Experience from "./components/Experience/Experience";
 import Projects from "./components/Projects/Projects";
 import Contact from "./components/Contact/Contact";
 import Footer from "./components/Footer/Footer";
+
 import "./app.scss";
 
 function App() {
