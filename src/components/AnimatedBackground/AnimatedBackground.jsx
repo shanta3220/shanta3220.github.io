4c081b4f54366fe731427d941612ef4195510fa0
NJShanta <njshanta@outlook.com>
2024-12-20 18:21:53 -0500

    Add cusdom cursor and fixed background animation

---
+++ src/components/AnimatedBackground/AnimatedBackground.jsx
@@ -0,0 +1,8 @@
+import { useEffect } from "react";
+import "./AnimatedBackground.scss";
+
+function AnimatedBackground() {
+  return <div className="animated-background"></div>;
+}
+
+export default AnimatedBackground;
