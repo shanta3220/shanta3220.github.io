bf1280b5d624e1599cd8dc3aaa9e5cfd7b8618ca
NJShanta <njshanta.ca@gmail.com>
2024-12-19 21:23:35 -0500

    add box background

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
 import About from "./components/About/About";
@@ -6,22 +7,27 @@ import Experience from "./components/Experience/Experience";
 import Projects from "./components/Projects/Projects";
 import Contact from "./components/Contact/Contact";
 import Footer from "./components/Footer/Footer";
+import MatrixCanvasBackground from "./components/MatrixCanvasBackground/MatrixCanvasBackground";
 
 import "./app.scss";
 
 function App() {
   return (
-    <div className="app">
-      <Header />
-      <main>
-        <Hero />
-        <About />
-        <Skills />
-        <Experience />
-        <Projects />
-        <Contact />
-      </main>
-      <Footer />
+    <div>
+      <div className="animated-background"></div>
+      <MatrixCanvasBackground />
+      <div className="content">
+        <Header />
+        <main>
+          <Hero />
+          <About />
+          <Skills />
+          <Experience />
+          <Projects />
+          <Contact />
+        </main>
+        <Footer />
+      </div>
     </div>
   );
 }
