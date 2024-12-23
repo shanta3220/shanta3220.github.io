4774b8a1fe3004d65419208f58ef04deefabd804
NJShanta <njshanta.ca@gmail.com>
2024-12-23 16:10:38 -0500

    add social to hero

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,8 +1,16 @@
 import { motion } from "framer-motion";
 import "./Hero.scss";
+import {
+  FaGithub,
+  FaLinkedin,
+  FaTwitter,
+  FaInstagram,
+  FaEnvelope,
+  FaArrowRight,
+} from "react-icons/fa";
 import { getTransition } from "../../scripts/utils";
 
-const Hero = () => {
+function Hero() {
   return (
     <section className="hero-section">
       <motion.h1 className="hero-section__header" {...getTransition(0.3)}>
@@ -17,8 +25,69 @@ const Hero = () => {
         specialize in building dynamic applications, tackling complex
         challenges, and collaborating across teams.
       </motion.p>
+      <motion.p className="hero-section__paragraph" {...getTransition(0.9)}>
+        Outside of coding, I enjoy exploring new technologies, reading
+        insightful articles, and staying inspired by data-driven innovation.
+      </motion.p>
+
+      <a href="#about-me" className="hero-section__see-more">
+        See more about me
+        <svg
+          className="hero-section__arrow-icon"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        >
+          <line x1="0" y1="12" x2="19" y2="12" />
+          <polyline points="12 5 19 12 12 19" />
+        </svg>
+      </a>
+      <div className="hero-section__social-links delayed-visible">
+        <SocialIcon
+          link="https://github.com/shanta3220"
+          icon={<FaGithub />}
+          label="GitHub"
+        />
+        <SocialIcon
+          link="https://linkedin.com/in/njshanta"
+          icon={<FaLinkedin />}
+          label="LinkedIn"
+        />
+        <SocialIcon
+          link="mailto:njshanta.ca@gmail.com"
+          icon={<FaEnvelope />}
+          label="Email"
+        />
+        <SocialIcon
+          link="https://instagram.com/njshanta"
+          icon={<FaInstagram />}
+          label="Instagram"
+        />
+        <SocialIcon
+          link="https://twitter.com/msnjshanta"
+          icon={<FaTwitter />}
+          label="Twitter"
+        />
+      </div>
     </section>
   );
-};
+}
+
+function SocialIcon({ link, icon, label }) {
+  return (
+    <a
+      href={link}
+      target="_blank"
+      rel="noopener noreferrer"
+      className="social-icon"
+    >
+      {icon} {label}
+    </a>
+  );
+}
 
 export default Hero;
