9e448d94e070ef2844cf1cb0b0658aa1e58514e4
NJShanta <njshanta.ca@gmail.com>
2024-12-28 03:40:46 -0500

    fix animations

--- src/scripts/utils.js
+++ src/scripts/utils.js
@@ -1,6 +1,11 @@
-export const getTransition = (delay) => ({
-  initial: { opacity: 0, y: 20, scale: 0.96 },
-  animate: { opacity: 1, y: 0, scale: 1 },
-  transition: { duration: 0.8, ease: "easeOut", delay },
-  exit: { opacity: 0, y: 20, scale: 0.96 },
-});
+export const getTransition = (delay, duration) => {
+  if (!duration) {
+    duration = 0.8;
+  }
+  return {
+    initial: { opacity: 0, y: 20, scale: 0.96 },
+    animate: { opacity: 1, y: 0, scale: 1 },
+    transition: { duration, ease: "easeOut", delay },
+    exit: { opacity: 0, y: 20, scale: 0.96 },
+  };
+};
