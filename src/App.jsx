c455430fab862bd940a03a5553eaec841b64e9ac
NJShanta <njshanta.ca@gmail.com>
2025-01-08 04:28:01 -0500

    fix naming issue

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
 import Projects from "./pages/Projects/Projects";
 import Contact from "./pages/Contact/Contact";
-import "./app.scss";
+import "./App.scss";
 
 function App() {
   return (
