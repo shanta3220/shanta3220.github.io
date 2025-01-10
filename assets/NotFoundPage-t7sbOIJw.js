a483f8d51b444c2f6d6b981334dac3732714971c
shanta3220 <shanta3220@users.noreply.github.com>
2025-01-10 06:08:50 +0000

    deploy: ff608815a520b0c91377089248a148384642afbd

---
+++ assets/NotFoundPage-t7sbOIJw.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-BKtVMb75.js";function r(){return t.jsx("div",{})}export{r as default};
---
+++ assets/ProjectsPage-BhBOiCKc.js
@@ -0,0 +1,98 @@
+import{r as l,j as i,m as r}from"./index-BKtVMb75.js";import{M as k,a as j}from"./MediaCard-CfHABCUp.js";import{g as c}from"./utils-CHFW8Wy8.js";const u=[{title:"JiffyGames",year:"Dec 2024",shortDescription:"Instant access to browser-based games with social features like leaderboards and profiles.",description:`Developed a Full-Stack web application as a solo project for my BrainStation capstone:
+      • Designed all page templates in Canva to follow as guidelines.
+      • Built a Vite-React frontend with pages like Login, Home, Game, Leaderboard pages.
+      • Integrated my own Unity3D WebGL games into the React application.
+      • Built a Node.js and Express.js backend with MySQL to manage users, games, leaderboards, and comments.
+      • Ensured smooth communication between frontend and backend using Axios.`,skills:["React.js","Vite","JavaScript","Sass","Node.js","Express.js","Knex.js","MySQL","Unity3D","Axios","C#","Git","Github","Task Prioritization"],media:[{type:"image",src:"/medias/jiffygames/jiffygames.png"}],category:"Web",website:"https://jiffygames.netlify.app",github:"https://github.com/shanta3220/jiffygames-frontend"},{title:"InStock",year:"Nov 2024",shortDescription:"A responsive Web platform for real-time warehouse and inventory tracking.",description:`Collaborated on a Full-Stack app as a developer:
+      • Built responsive Warehouse Details and Add Warehouse pages.
+      • Created MySQL database and backend endpoints for warehouse management.
+      • Tested the frontend UI for seamless user experience.`,skills:["JavaScript","Vite","Jira","React.js","Sass","React Router Dom","Node.js","Express.js","MySQL","Knex.js","Axios","Git","Postman","Team Collaboration","Task Prioritization"],media:[{type:"image",src:"/medias/instock/instock.png"}],category:"Web",website:"https://njshanta-instock.netlify.app",github:"https://github.com/shanta3220/inStock"},{title:"Just for Laughs",year:"Nov 2024",shortDescription:"App generating personalized images based on user inputs using OpenAI.",description:`Built during a 24-hour hackathon judged by Meta panelists as part of a team:
+      • Collaborated on a React and Node.js app to create personalized AI-powered images based on user hobbies, age, mood, and humor type.
+      • Designed a MySQL database to store jokes and categories.
+      • Created backend endpoints for seamless data integration and debugged frontend UI issues to enhance usability.
+      • Ensured smooth communication between frontend and backend using Axios.`,skills:["React.js","JavaScript","Sass","Node.js","Express.js","MySQL","OpenAI","Axios","AI Integration","Git","Team Collaboration","Problem-Solving"],media:[{type:"image",src:"/medias/just_for_laughs/just_for_laughs.jpg"}],category:"Web",github:"https://github.com/shanta3220/ip-meta-client"},{title:"BrainFlix",year:"Oct 2023",shortDescription:"Responsive platform with video playback, comments, and dynamic rendering.",description:`Built key features for a video streaming app:
+      • Developed Home and Upload pages with video playback and form validation.
+      • Created backend routes for GET and POST requests.
+      • Ensured dynamic content rendering and smooth user experience.`,skills:["JavaScript","Sass","Vite","Axios","React.js","Node.js","Express.js","Github","OOP"],media:[{type:"image",src:"/medias/brainflix/brainflix.png"}],category:"Web",website:"https://njshanta-brainflix.netlify.app",github:"https://github.com/shanta3220/nusrat-jahan-shanta-brainflix"},{title:"CoffeeShop",year:"Sep 2023",shortDescription:"Static, browser-based website providing users with information about premium coffee products.",description:`Created a 2-page static website:
+      • Designed layouts using HTML5 and CSS, following BEM conventions.
+      • Ensured proper hover states and CSS animations for interactivity.`,skills:["HTML","CSS"],media:[{type:"image",src:"/medias/coffeeshop/coffeeshop.png"}],category:"Web",website:"https://njshanta-coffeeshop.netlify.app",github:"https://github.com/shanta3220/nusrat-jahan-shanta-coffeeshop"},{title:"BandSite",year:"Oct 2023",shortDescription:"Responsive website with DOM manipulation and API integration.",description:`Developed a dynamic website:
+      • Built Home and Shows pages using JavaScript, HTML, and Sass.
+      • Integrated API for dynamic content with GET, POST, and DELETE requests.
+      • Ensured interactivity and responsive design.`,skills:["JavaScript","Axios","HTML","Sass"],media:[{type:"image",src:"/medias/bandsite/bandsite.gif"}],category:"Web",website:"https://njshanta-bandsite.netlify.app",github:"https://github.com/shanta3220/nusrat-jahan-shanta-bandsite"},{title:"Dishify",year:"Oct 2024",shortDescription:"A web app to fetch recipes based on selected ingredients using the Spoonacular API.",description:`Developed during a 5.5-hour Web API Hackathon at BrainStation:
+      • Built a web application using the Spoonacular API to fetch recipes based on selected ingredients.
+      • Designed and implemented backend logic for managing API requests and ingredient combinations.
+      • Collaborated with teammates to ensure smooth data flow and integration with the frontend.`,skills:["JavaScript","HTML","CSS","Spoonacular API","API Integration","Git","Problem-Solving","Team Collaboration"],media:[{type:"image",src:"/medias/dishify/dishify.gif"}],website:"https://dishifyweb.netlify.app",github:"https://github.com/shanta3220/Dishify",category:"Web"},{title:"Nimoyd",year:"Feb 2020 - June 2024",shortDescription:"Implemented UIs for an adventure top-down sandbox game made with Unity3D",skills:["Unity3D","C#","Trello","Github","SourceTree","Team Collaboration"],category:"Games",media:[{type:"video",src:"/medias/nimoyd/trailer.webm",title:"Trailer of the game - Nimoyd",shortDescription:"An adventure top-down sandbox game made with Unity3D",longDescription:`My contributions include:
+           • Implemented UI into the game, iterating multiple times to enhance usability and visual consistency.
+           • Completed 949 tasks in Trello—the highest in the team due to long tenure.
+           • Mentored a junior developer to contribute significantly to key game features.
+           • Worked with a team of over 30 contributors, including current and former members, to ensure design consistency and cohesive execution.
+           • Resolved 30% of reported bugs, improving game stability, and conducted code reviews to ensure high-quality code.`,year:"Feb 2020 - Jun 2024"},{type:"image",src:"/medias/nimoyd/main_ui.png",title:"Main Screen",shortDescription:"Default main screen during gameplay.",longDescription:`Implemented core UI elements for gameplay:
+          • Added quick item slots at the bottom with keyboard shortcuts.
+          • Developed bottom-left mouseover voxel item name, dynamically updating based on mouse position.
+          • Created health bar and notification cells on the left.
+          • Interactive behavior for the right minimap and clock.
+          • Ensured responsive design for multiple devices.`,year:"2022"},{type:"image",src:"/medias/nimoyd/world_map_ui.jpg",title:"World Map Screen",shortDescription:"Clickable building icons with details on interaction.",longDescription:`Implemented interactivity for the world map:
+          • Updated building icons and ensured dynamic interactions.
+          • Integrated functionality to display detailed information on click.
+          • Ensured responsiveness across devices.`,year:"2023"},{type:"image",src:"/medias/nimoyd/npc_conversation_ui.png",title:"NPC Quest Screen",shortDescription:"Interact with NPCs and view their details.",longDescription:`Implemented NPC Quest UI features:
+          • Built the UI from scratch with responsive design.
+          • Added animations for interactive elements.
+          • Ensured camera alignment with NPCs during interactions.`,year:"2022"},{type:"image",src:"/medias/nimoyd/worker_selection_ui.png",title:"Worker Selection Screen",shortDescription:"Displays Worker NPCs in a circular grid.",longDescription:`Implemented dynamic worker selection features:
+          • Created UI to update the worker list based on active category.
+          • Added building detail information on the right.
+          • Ensured responsive design across devices.`,year:"2023"},{type:"image",src:"/medias/nimoyd/world_creation_ui.jpg",title:"World Creation Screen",shortDescription:"UI for creating a new game world.",longDescription:`Implemented features for world creation:
+          • Added dynamic updates for info based on categories such as General and Weather.
+          • Ensured functionality of all buttons.
+          • Modified backend logic to support seamless UI interactions.`,year:"2023"},{type:"image",src:"/medias/nimoyd/character_ui.jpg",title:"Character Screen",shortDescription:"Track character progress and stats.",longDescription:`Implemented character UI:
+          • Included user's current progress and stats upgrade features.
+          • Added UI animations for interactivity.
+          • Ensured the UI was fully responsive across devices.`,year:"2021"},{type:"image",src:"/medias/nimoyd/character_selection_ui.webp",title:"Character Creation Screen",shortDescription:"Select a character to play in the game, set traits and gender.",longDescription:`Implemented character customization features:
+          • Built a dynamic UI for character creation.
+          • Enabled dynamic updates for traits and gender selection.
+          • Ensured data saved to the game database and responsive design.`,year:"2022"},{type:"image",src:"/medias/nimoyd/social_journal_ui.jpg",title:"Social Journal Screen",shortDescription:"Track Quest NPCs based on factions.",longDescription:`Implemented faction-based journal features:
+          • Built a grid-based UI to dynamically display NPC profiles by faction.
+          • Ensured consistent layout and responsive design.`,year:"2022"},{type:"image",src:"/medias/nimoyd/excavation_site_ui.jpg",title:"Excavation Site Screen",shortDescription:"Excavate different excavation site buildings.",longDescription:`Implemented excavation UI with stage-based interactions:
+          • Developed a UI stage system for excavated, available, and locked states.
+          • Ensured the design dynamically adapts based on active stage.
+          • Made the UI fully responsive.`,year:"2021"},{type:"image",src:"/medias/nimoyd/building_mode_ui.png",title:"Building Grid Screen",shortDescription:"Create templates or place buildings in the game world.",longDescription:`Implemented the building grid UI:
+          • Developed the UI to display buildings based on categories and subcategories.
+          • Updated item details dynamically based on user selections and interactions.
+          • Ensured full responsiveness across devices.`,year:"2022"},{type:"image",src:"/medias/nimoyd/building_crafting_ui.png",title:"Building Details Screen",shortDescription:"Craft items and generate energy in buildings.",longDescription:`Implemented the building details UI:
+          • Added features to update recipes and player inventory based on the building's category.
+          • Developed the UI to display crafting progress and item details dynamically.
+          • Ensured smooth interactions for crafting actions and item generation.
+          • Made the UI fully responsive across devices.`,year:"2022"},{type:"image",src:"/medias/nimoyd/journal_ui.jpg",title:"Journal Screen",shortDescription:"Check conversation logs and quest progress.",longDescription:`Implemented journal tracking system:
+          • Built a responsive journal UI where quests and notes update dynamically based on user actions.
+          • Ensured consistent layout and synchronized data with user progress.`,year:"2021"}]},{title:"Riko: The Adventurer",year:"Jan 2019",shortDescription:"A 2D top-down RPG shooting game developed as a final year project for my Bachelor of Science degree.",description:`Final year project for my Bachelor of Science degree:
+      • Developed a 2D RPG shooting game in Unity3D with character progression and multiple enemies.
+      • Integrated music, sound effects, animations, and advanced gameplay mechanics.
+      • Documented daily progress in Google Docs for supervisor review.`,skills:["Unity3D","C#","Git","Google Docs"],media:[{type:"video",thumbnail:"/medias/riko_the_adventurer/riko_the_adventurer.gif",src:"/medias/riko_the_adventurer/riko_the_adventurer.mp4"}],category:"Games",github:"https://github.com/shanta3220/Riko-The-Adventurer"},{title:"LSD: Last Survival Days",year:"Nov 2018",shortDescription:"A top-down shooting game developed for a band's debut album.",description:`Developed a top-down shooting game in Unity3D for fan engagement:
+        • Designed gameplay, levels, and interactive UI elements.
+        • Wrote scripts for player movement, enemies, and camera behavior.
+        • Integrated Google Leaderboard for enhanced player competition.`,skills:["Unity3D","C#","Team Collaboration"],media:[{type:"video",thumbnail:"/medias/lsd_last_survival_days/lsd_last_survival_days.gif",src:"/medias/lsd_last_survival_days/lsd_last_survival_days.mp4"}],category:"Games"},{title:"Lana's Adventure",year:"Nov 2017",shortDescription:"A 2D platformer game developed as the sole game developer for MU Tech Fest 2017.",description:`Developed as the runner-up entry for the MU Tech Fest:
+      • Created the game from scratch in Unity3D alongside a graphic designer.
+      • Wrote scripts for player movement, enemies, platforms, and camera behavior.
+      • Designed and implemented levels, integrating interactive UI elements.`,skills:["Unity3D","C#","Team Collaboration"],media:[{type:"video",thumbnail:"/medias/lanas_adventure/lanas_adventure.gif",src:"/medias/lanas_adventure/lanas_adventure.mp4"}],category:"Games"},{title:"Cute Puppy Love",year:"2020",shortDescription:"An interactive mobile game where players care for, play with, and feed virtual puppies.",description:`Developed an engaging mobile game in Unity3D where players interact with virtual puppies:
+        • Designed and implemented core gameplay mechanics, including playing, feeding, and selecting puppies.
+        • Integrated responsive UI elements for seamless gameplay on mobile devices.
+        • Collaborated with a designer to finalize UI assets and animations.
+        • Ensured a smooth and enjoyable player experience through meticulous testing and iteration.`,skills:["Unity3D","C#","Team Collaboration"],media:[{type:"image",src:"/medias/cute_puppy_love/puppy_selection.jpeg",title:"Puppy Selection Screen",shortDescription:"Select the puppy's breed.",longDescription:`Implemented the Puppy Selection UI:
+            • Created the initial mockup UI, which served as the foundation for the designer's final implementation.
+            • Replaced the mockup with final UI assets.
+            • Added animated puppy models.
+            • Ensured responsiveness across mobile devices.`,year:"2020"},{type:"image",src:"/medias/cute_puppy_love/puppy_play.jpeg",title:"Puppy Play Screen",shortDescription:"Play with the puppy.",longDescription:`Implemented the gameplay and UI:
+            • Created ball-throwing mechanics using drag and release gestures.
+            • Integrated and maintained the animation controller.
+            • Ensured responsiveness across mobile devices.`,year:"2020"},{type:"image",src:"/medias/cute_puppy_love/puppy_feed.jpeg",title:"Puppy Feed Screen",shortDescription:"Feed the puppy.",longDescription:`Implemented the gameplay and UI:
+            • Added various food items with unique energy levels.
+            • Implemented energy restoration mechanics when the puppy eats.
+            • Ensured responsiveness across mobile devices.`,year:"2020"}],category:"Games"},{title:"Quiz Race",year:"Jan 2018",shortDescription:"A puzzle-based autorun game where players guide a car into the correct lane matching the answer.",description:`Developed a puzzle-based autorun game in Unity3D:
+        • Designed gameplay mechanics where players guide a car into the correct lane corresponding to the answer.
+        • Collaborated with a Game Artist to create engaging visuals and animations.
+        • Implemented smooth lane-switching mechanics and dynamic question generation for replayability.`,skills:["Unity3D","C#","Game Mechanics","Collaboration"],media:[{type:"image",src:"/medias/quiz_race/quiz_race.gif"}],category:"Games"},{title:"AR Bornoporichoy",year:"July 2018",shortDescription:"An educational AR app to teach Bengali alphabets, built for the Intra MU Tech Emulation V1 competition.",description:`Showcased as the champion project at the Intra MU Tech Emulation:
+      • Set up the Vuforia AR package in Unity3D.
+      • Developed a reusable audio controller for 3D models of Bengali alphabets.
+      • Populated 3D objects and conducted rigorous testing to ensure a smooth user experience.`,skills:["Unity3D","C#","Team Collaboration"],media:[{type:"video",thumbnail:"/medias/intra_mu/bornoporichoygif.gif",src:"/medias/intra_mu/video_1.mp4"}],category:"General"},{title:"VR Webb",year:"Oct 2019",shortDescription:"A VR educational app showcasing the James Webb Space Telescope, created for NASA Space Apps Challenge 2019.",description:`Developed as the Global Nominee for NASA Space Apps Challenge:
+      • Lead Unity3D programmer, setting up Google Cardboard VR SDK and CurvedUI.
+      • Created interactive UI elements to showcase the telescope’s instruments and features.
+      • Synchronized animations with a timeline UI to narrate the telescope's journey.`,skills:["Unity3D","C#","Team Collaboration"],media:[{type:"video",src:"/medias/vr_webb_2019/video1.mp4",thumbnail:"/medias/vr_webb_2019/thumbnail.png"}],category:"General",website:"https://2019.spaceappschallenge.org/challenges/stars/and-away/teams/vr_webb/project"}],p=["All",...new Set(u.map(n=>n.category))],U=n=>p?n?n==="All"?u:u.filter(o=>o.category===n):(p.console.error("category is undefined"),[]):(p.warn("no categories, add categories to projects"),[]),A=()=>p,M=()=>{const[n,o]=l.useState(null),[a,h]=l.useState([]),[y,d]=l.useState(null),[b,v]=l.useState("All"),m=(e,t)=>({...e,title:e.title||t.title,shortDescription:e.shortDescription||t.shortDescription,longDescription:e.longDescription||t.description,skills:e.skills||t.skills,github:e.github||t.github,website:e.website||t.website,year:e.year||t.year}),D=(e,t,s)=>{const g=t[s];o(m(g,e)),h(t),d(s)},w=()=>{o(null),h([]),d(null)},S=()=>{if(!a||a.length===0)return;const e=(y+1)%a.length,t=a[e];d(e),o(s=>m(t,s))},_=()=>{if(!a||a.length===0)return;const e=(y-1+a.length)%a.length,t=a[e];d(e),o(s=>m(t,s))},I=A(),C=U(b);return i.jsxs(r.div,{className:"projects",...c(0,1),children:[i.jsx(r.h1,{className:"projects__title",...c(.2,.8),children:"My Projects"}),i.jsx(r.div,{className:"projects__categories",...c(.2,.8),children:I.map(e=>i.jsx("button",{className:`projects__category ${b===e?"active":""}`,onClick:()=>v(e),children:e},e))}),i.jsx(r.div,{className:"projects__grid",...c(.3,.8),children:C.map((e,t)=>{var s;return i.jsxs(r.div,{className:"projects__card",whileHover:{scale:1.04},whileTap:{scale:.95},children:[((s=e.media)==null?void 0:s.length)>0&&e.media.map((g,f)=>i.jsx(k,{media:g,onClick:()=>D(e,e.media,f)},`${t}-${f}-${e.title}`)),i.jsxs("div",{className:"projects__card-overlay",children:[i.jsx("h3",{className:"projects__card-title",children:e.title}),i.jsx("p",{className:"projects__card-description",children:e.shortDescription})]})]},t)})}),i.jsx(j,{isOpen:!!n,content:n,onClose:w,onNext:S,onPrevious:_,showNextPreviousButton:(a==null?void 0:a.length)>1})]})};export{M as default};
