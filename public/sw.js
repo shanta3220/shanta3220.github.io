49bf5645291fdd39c2492d09d5b209bcdb6caea0
NJShanta <njshanta.ca@gmail.com>
2025-01-10 02:35:57 -0500

    removed unnecessary code from index.html

--- public/sw.js
+++ public/sw.js
@@ -1,9 +1,17 @@
-self.addEventListener("fetch", (event) => {
-  if (event.request.mode === "navigate") {
-    event.respondWith(
-      caches.match("/index.html").then((cached) => {
-        return cached || fetch("/index.html");
-      })
-    );
-  }
-});
+// self.addEventListener("fetch", (event) => {
+//   if (event.request.mode === "navigate") {
+//     event.respondWith(
+//       caches.match("/index.html").then((cached) => {
+//         return cached || fetch("/index.html");
+//       })
+//     );
+//   }
+// });
+
+// <script>
+// if ("serviceWorker" in navigator) {
+//   navigator.serviceWorker.register("/sw.js").catch((err) => {
+//     console.error("Service Worker registration failed:", err);
+//   });
+// }
+// </script>
