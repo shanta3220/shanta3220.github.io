a3540a10bc70f8802ea61accbe4baf629ae788b6
shanta3220 <shanta3220@users.noreply.github.com>
2025-03-03 16:16:30 +0000

    deploy: ae264bf7dabfbd4c5920bdb8dab4137ad3448792

--- assets/ProjectsPage-DAkv4OAl.js
+++ assets/ProjectsPage-DAkv4OAl.js
@@ -1,4 +1,4 @@
-import{r as l,j as i,m as r}from"./index-BhpvtmDy.js";import{M as k,a as j}from"./MediaCard-DXOF-RaD.js";import{g as c}from"./utils-DO6f8zla.js";const u=[{title:"JiffyGames",year:"Dec 2024",shortDescription:"Instant access to browser-based games with social features like leaderboards and profiles.",description:`Developed a Full-Stack web application as a solo project for my BrainStation capstone:
+import{r as l,j as i,m as r}from"./index-CZnWzN2a.js";import{M as k,a as j}from"./MediaCard-DQvqTQkg.js";import{g as c}from"./utils-B94lsQbb.js";const u=[{title:"JiffyGames",year:"Dec 2024",shortDescription:"Instant access to browser-based games with social features like leaderboards and profiles.",description:`Developed a Full-Stack web application as a solo project for my BrainStation capstone:
       • Designed all page templates in Canva to follow as guidelines.
       • Built a Vite-React frontend with pages like Login, Home, Game, Leaderboard pages.
       • Integrated my own Unity3D WebGL games into the React application.
